abf25cd934988c96b6eda3a2a4d45e07793a2a2c
peeplil6666@gmail.com <peeplil6666@gmail.com>
2024-05-11 16:34:36 +0200

    added api /Laravel santcum

---
+++ routes/api.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+ /*
+Route::get('/posts', function(){
+    return response()->json([
+        'Title' => 'Post one' 
+    ]);
+});
+*/
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,25 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+/* NOTES ONLY DONT UNCOMMENT ANY OF THIS
+
+Route::get('/hello', function (){
+   return response("Hello World", 200);
+});
+
+//wild cards
+Route::get('/posts/{id}', function($id){
+    return response( 'Post ' . $id);
+})->where('id' , '[0-9]+'); // regex constraint 
+ 
+// Request Query param display?nom=John&prenom=Doe
+Route::get('/display', function(Request $request){
+    return $request->nom . ' ' . $request->prenom;
+});
+*/
