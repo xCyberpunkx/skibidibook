561029495e10dc3e393d0cfca38e5a762c56112d
xCyberpunkx <peeplil6666@gmail.com>
2024-05-12 11:57:24 +0200

    tags logic fixed for future filtring

--- resources/views/components/gig-card.blade.php
+++ resources/views/components/gig-card.blade.php
@@ -12,28 +12,8 @@
                             </h3>
                             <div class="text-xl font-bold mb-4">{{ $gig->company }}</div>
 
-                            <ul class="flex">
-                                <li
-                                    class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs"
-                                >
-                                    <a href="#">Laravel</a>
-                                </li>
-                                <li
-                                    class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs"
-                                >
-                                    <a href="#">API</a>
-                                </li>
-                                <li
-                                    class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs"
-                                >
-                                    <a href="#">Backend</a>
-                                </li>
-                                <li
-                                    class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs"
-                                >
-                                    <a href="#">Vue</a>
-                                </li>
-                            </ul>
+                            <x-tags :tagsCsv="$gig->tags" />
+                            
 
                             <div class="text-lg mt-4">
                                 <i class="fa-solid fa-location-dot"></i> 
